0dd5f85667ceaa6c949bc4bee32e0f533ed41e8b
Dave Cooper <dave@davecooper.org>
2014-06-23 20:45:57 +0800

    Implemented POST. Ruby sucks.

--- client.rb
+++ client.rb
@@ -32,7 +32,7 @@ class Client
        return make_api_call() 
     end
 
-    def get_orders(params = nil)
+    def get_orders(params = {})
         return make_api_call("orders", params)
     end
 
@@ -48,11 +48,11 @@ class Client
         return make_api_call("orders/#{order_id}/notes")
     end
 
-    def update_order(order_id, data = nil)
-        return make_api_call("orders/#{order_id}", data, 'POST')
+    def update_order(order_id, params = {})
+        return make_api_call("orders/#{order_id}", params, 'POST')
     end
 
-    def get_coupons(params = nil)
+    def get_coupons(params = {})
         return make_api_call("coupons", params)
     end
 
@@ -65,11 +65,10 @@ class Client
     end
 
     def get_coupon_by_code(coupon_code)
-        # TODO - need to escape characters here?
         return make_api_call("coupons/code/#{coupon_code}") 
     end
 
-    def get_customers(params = nil)
+    def get_customers(params = {})
         return make_api_call("customers", params)
     end
 
@@ -101,23 +100,19 @@ class Client
         return make_api_call("products/count")
     end
 
-    def get_reports(params = nil)
+    def get_reports(params = {})
         return make_api_call("reports")
     end
 
-    def get_sales_report(params = nil)
+    def get_sales_report(params = {})
         return make_api_call("reports/sales", params)
     end
 
-    def get_top_sellers_report(params = nil)
+    def get_top_sellers_report(params = {})
         return make_api_call("reports/sales/top_sellers", params)
     end
 
-    def make_api_call(endpoint, params = nil, method='GET')
-        if (params.empty?)
-            params = {}
-        end
-        
+    def make_api_call(endpoint, params = {}, method='GET')
         if (!@is_ssl)
             params[:oauth_consumer_key] = @consumer_key
             params[:oauth_nonce] = Digest::SHA1.hexdigest(Time.new.to_i)
@@ -135,17 +130,13 @@ class Client
             req.basic_auth(@consumer_key, @consumer_secret)
             res = http.start { |test| test.request(req) }
         elsif (method == HTTP_POST)
-            # TODO - make POST work
-            uri = URI.parse(@api_url)
-            http = Net::HTTP.new(uri.host, uri.port)
-            http.use_ssl = true
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-            req = Net::HTTP::Post.new(endpoint)
+            url = URI.parse(@api_url + endpoint)
+            req = Net::HTTP::Post.new(url.path)
             req.basic_auth(@consumer_key, @consumer_secret)
-            req.add_field("Content-Type", "application/json")
-            req.body(params)
-            response = http.request(req)
-            puts response.body
+            req.body = params.to_json
+            sock = Net::HTTP.new(url.host, url.port)
+            sock.use_ssl = true
+            res = sock.start { |test| test.request(req) }
         else
             raise "Unsupported HTTP operation requested"    
         end
